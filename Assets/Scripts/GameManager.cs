0198f9252e5c7f2d90d77e4ed28bfff762a514fa
unknown <emarkle99@gmail.com>
2024-04-14 23:54:39 -0500

    fleshed out gamemanager - incomplete

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,11 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
+    public int stage { get; private set; }
+    public int lives { get; private set; }
     private void Awake()
     {
         if (Instance != null) {
@@ -17,6 +20,52 @@ public class GameManager : MonoBehaviour
         if (Instance == this) {
             Instance = null;
         }
+    }
+
+    private void Start()
+    {
+        NewGame();
+    }
+
+    private void NewGame()
+    {
+        lives = 3;
+
+        LoadLevel();
+    }
+
+    private void LoadLevel(int stage)
+    {
+        this.stage = stage;
+
+        SceneManager.LoadScene($"{stage}");
+    }
+
+    public void NextLevel()
+    {
+        // ... LoadLevel(stage + 1);
+    }
+
+    public void ResetLevel(float delay)
+    {
+        Invoke(nameof(ResetLevel), delay);
+    }
+
+    public void ResetLevel()
+    {
+        lives--;
+
+        if (lives > 0) {
+            LoadLevel(stage);
+        } else {
+            GameOver();
+        }
+    }
+
+    private void GameOver()
+    {
+        NewGame();
+        // SceneManager.LoadScene("GameOver")
     }
 
 }
