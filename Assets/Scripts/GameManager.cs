9efe0818cee9215f48d4cc149aefb3a70ef617ab
unknown <emarkle99@gmail.com>
2024-04-14 23:40:33 -0500

    simple game manager singleton

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,4 +2,21 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance { get; private set; }
+    private void Awake()
+    {
+        if (Instance != null) {
+            DestroyImmediate(gameObject);
+        }   else {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+    private void OnDestroy()
+    {
+        if (Instance == this) {
+            Instance = null;
+        }
+    }
+
 }
